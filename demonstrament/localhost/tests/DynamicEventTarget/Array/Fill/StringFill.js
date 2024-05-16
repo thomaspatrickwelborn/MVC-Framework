ec5b544cd9ad8b2de57c259d817e00f2d71b9ce7
Thomas Patrick Welborn <thomas.patrick.welborn@outlook.com>
2024-05-15 18:15:51 -0700

    Array Fill Test

--- demonstrament/localhost/tests/DynamicEventTarget/Array/Fill/StringFill.js
+++
@@ -1,8 +0,0 @@
-const StringFill = ($character, $length) => {
-  const strings = []
-  strings.length = $length
-  strings.fill($character, 0, $length)
-  return strings.join('')
-}
-
-export default StringFill
--- demonstrament/localhost/tests/DynamicEventTarget/Array/Fill/index.js
+++ demonstrament/localhost/tests/DynamicEventTarget/Array/Fill/index.js
@@ -1,55 +1,16 @@
 import DynamicEventTarget from '/mvc-framework//Core/DynamicEventTarget/index.js'
-import Values from './Values.js'
-import Ranges from './Ranges.js'
 
 export default function ArrayFillTest() {
-  // Iterate Range Groups
-  iterateRangeGroups:
-  for(const [
-    $rangeName, $ranges
-  ] of Object.entries(Ranges)) {
-    // Iterate Ranges
-    iterateRanges:
-    for(const $range of $ranges) {
-      const [$length, $start, $end] = $range
-      // Iterate Value Types
-      iterateValueTypes: 
-      for(const [
-        $valueType, $values
-      ] of Object.entries(Values)) {
-        // Iterate Values
-        iterateValues: 
-        for(const $value of $values) {
-          // Test Array Fill
-          const array = []
-          array.length = $length
-          array.fill($value, $start, $end)
-          const lengthAssertion = array.length === $length
-          console.assert(
-            lengthAssertion,
-            [
-              `array.length !== $length`,
-              `${array.length} !== ${$length}`,
-              `${array.length === $length}`,
-            ].join('\n')
-          )
-          var arrayIndex = 0
-          while(arrayIndex < array.length) {
-            let arrayItem = array.at(arrayIndex)
-            const itemAssertion = arrayItem === $value
-            console.log(arrayItem, $value)
-            console.assert(
-              arrayItem === $value,
-              [
-                [`arrayItem (${arrayIndex}) value should be `, $value].join(''),
-                [`arrayItem (${arrayIndex}) is `, arrayItem].join(''),
-              ].join('\n')
-            )
-            arrayIndex++
-          }
-        }
-      }
-    }
-  }
-  return {}
+  var detArray = new DynamicEventTarget([])
+  detArray.addEventListener('fillIndex', ($event) => {
+    console.log($event.type, $event.detail)
+  })
+  detArray.addEventListener('fill', ($event) => {
+    console.log($event.type, $event.detail)
+  })
+  detArray.length = 100
+  console.log(detArray.fill({a:1,b:2,c:3}, 0, 100))
+  var array = new Array([])
+  array.length = 100
+  console.log(array.fill({a:1,b:2,c:3}, 0, 100))
 }
