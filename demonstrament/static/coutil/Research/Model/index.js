2500d95867c34ef57ecd0b1e085d95660fbb1f80
Thomas Patrick Welborn <thomas.patrick.welborn@outlook.com>
2024-09-05 13:21:13 -0700

    Revert "Dynamic Event Target Tests"
    This reverts commit 2c4eb50d1366659f4ae471d0a706d459ff9d9f38.

--- demonstrament/static/coutil/Research/Model/index.js
+++ demonstrament/static/coutil/Research/Model/index.js
@@ -27,4 +27,9 @@ export default class Research extends EventTarget {
     }
     return this.#_topics
   }
+  get parent() {
+    if(this.#_parent !== undefined) return this.#_parent
+    this.#_parent = this.#settings.parent
+    return this.#_parent
+  }
 }
--- demonstrament/static/coutil/Research/index.js
+++ demonstrament/static/coutil/Research/index.js
@@ -2,5 +2,6 @@ import Model from './Model/index.js'
 export default class Model extends EventTarget {
   constructor($settings) {
     super()
+    
   }
 }
