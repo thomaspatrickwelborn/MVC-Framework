3af018d9e24d95646a5b3b472a4dd2d82ac46fe5
Thomas Patrick Welborn <thomas.patrick.welborn@outlook.com>
2024-09-25 17:21:57 -0700

    Update sass.piler.js

--- demonstrament/application/pilers/sass.piler.js
+++ demonstrament/application/pilers/sass.piler.js
@@ -1,8 +1,25 @@
+import path from 'node:path'
 import createDir from '../coutil/createDir/index.js'
 import * as sass from 'sass'
 import { stat, mkdir, writeFile } from 'node:fs/promises'
 export default async function SassPiler($settings) {
   await createDir($settings.output)
-  const sassPile = sass.compile($settings.input)
-  await writeFile($settings.output, sassPile.css)
+  const sourceMapExtension = '.css.map'
+  const sassPilePath = $settings.output
+  const sourceMapFilePath = [
+    '.', path.basename($settings.output).concat('.map')
+  ].join('/')
+  const sassPileSourceMapPend = `'\n/*# sourceMappingURL=${sourceMapFilePath} */`
+  console.log(sassPileSourceMapPend)
+  const sassPileSourceMapPath = $settings.output.replace(
+    new RegExp(/\.css$/), sourceMapExtension
+  )
+  var sassPile = sass.compile($settings.input, {
+    sourceMap: true,
+    sourceMapIncludeSources: true,
+  })
+  const sassPileCSS = sassPile.css.concat(sassPileSourceMapPend)
+  const sassPileSourceMap =JSON.stringify(sassPile.sourceMap)
+  await writeFile(sassPilePath, sassPileCSS)
+  await writeFile(sassPileSourceMapPath, sassPileSourceMap)
 }
