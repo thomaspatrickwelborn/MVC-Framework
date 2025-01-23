05a6b21cc860854de762661381a14a606e973ef6
Thomas Patrick Welborn <thomas.patrick.welborn@outlook.com>
2025-01-23 10:48:37 -0800

    MVC Framework Demonstrament Is DPM Development

--- demonstrament/application/coutil/createDir/index.js
+++
@@ -1,7 +0,0 @@
-import path from 'node:path'
-import { mkdir, stat } from 'node:fs/promises'
-export default async function createDir($path) {
-  const outputDir = path.dirname($path)
-  try { await stat(outputDir) }
-  catch { await mkdir(outputDir, { recursive: true }) }
-}
--- demonstrament/index.js
+++ demonstrament/index.js
@@ -1,6 +1,1 @@
-import Application from './application/index.js'
-import ApplicationConfig from './application.config.js'
-const application = await new Promise(($resolve, $reject) => {
-  const _application = new Application(ApplicationConfig)
-  _application.on('ready', $resolve(_application))
-})
+console.log("MVC Framework Demonstrament")
